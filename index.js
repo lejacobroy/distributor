2432737eb5df8d46facc1460b31800ee9583bf37
Jacob Roy <lejacobroy@gmail.com>
2018-08-11 20:56:40 -0400

    few fixes

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 var rpio = require('rpio');
+
+var date = new Date();
+var current_hour = date.getHours();
+
+var pin = 11;
+var steps = 50;
+
+/*init done*/
+
+/*init GPIO*/
 rpio.init({gpiomem: true});    /* Use /dev/mem for i²c/PWM/SPI */
 rpio.init({mapping: 'gpio'});   /* Use the GPIOxx numbering */
 rpio.on('warn', function() {});
+rpio.close(pin, rpio.PIN_RESET);
+
 /*
  * Set the initial state to low.  The state is set prior to the pin
  * being actived, so is safe for devices which require a stable setup.
  */
- var pin = 19;
- var steps = 50
+
 rpio.open(pin, rpio.OUTPUT, rpio.LOW);
+
 
 /*
  * The sleep functions block, but rarely in these simple programs does
@@ -18,7 +30,7 @@ rpio.open(pin, rpio.OUTPUT, rpio.LOW);
 for (var i = 0; i < steps; i++) {
         /* On for 1 second */
         rpio.write(pin, rpio.HIGH);
-
+        rpio.usleep(3); 
         /* Off for half a second (500ms) */
         rpio.write(pin, rpio.LOW);
 }
