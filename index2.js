f49b1f2a79a1b7e58ecbe57cee5bb52fe2b2a4df
Jacob Roy <lejacobroy@gmail.com>
2018-08-27 20:53:28 -0400

    adds timers

--- index2.js
+++ index2.js
@@ -50,9 +50,7 @@ rpio.open(pin4, rpio.OUTPUT, rpio.LOW);
         rpio.write(pin2, rpio.LOW);
 }
 */
-//if((current_hour == 3 || current_hour == 6 || current_hour == 9 || current_hour == 12 || current_hour == 15 || current_hour == 18 || current_hour == 21 || current_hour == 24) & current_minutes == 00){
 
-//while(1){
 function forward(){
 	for (var y = 0; y <= steps; y++) {
 		if (i==7) {
@@ -219,18 +217,23 @@ rpio.close(pin3, rpio.PIN_RESET);
 rpio.close(pin4, rpio.PIN_RESET);
 }
 
-reverse();
-reset();
-sleep.sleep(1);
+if((current_hour == 3 || current_hour == 6 || current_hour == 9 || current_hour == 12 || current_hour == 15 || current_hour == 18 || current_hour == 21 || current_hour == 24) & current_minutes == 00){
 
-forward();
-reset();
-sleep.sleep(1);
+	reverse();
+	reset();
+	sleep.sleep(1);
 
-reverse();
-reset();
-sleep.sleep(1);
+	forward();
+	reset();
+	sleep.sleep(1);
 
-forward();
-reset();
-sleep.sleep(1);
+	reverse();
+	reset();
+	sleep.sleep(1);
+
+	forward();
+	reset();
+	sleep.sleep(1);
+
+	sleep.sleep(60)
+}
